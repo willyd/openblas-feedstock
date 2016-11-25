77a5dc9aa825bbde5924af5cf55fa5b6b2a3a4b8
Guillaume Dumont <dumont.guillaume@gmail.com>
2016-11-25 14:32:40 -0500

    Added windows support with binaries

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,8 @@
+copy %SRC_DIR%\bin\* %LIBRARY_BIN%
+copy %SRC_DIR%\include\* %LIBRARY_INC%
+xcopy %SRC_DIR%\lib\* /S %LIBRARY_LIB%
 
-:: Set $HOME to the current dir so msys runs here
-set HOME=%cd%
+del %LIBRARY_LIB%\libopenblas.a
+move %LIBRARY_LIB%\libopenblas.dll.a %LIBRARY_LIB%\libopenblas.lib
 
-:: Configure, build, test, and install using `make`.
-bash -lc "make FC=gfortran DYNAMIC_ARCH=1 BINARY=$ARCH NO_LAPACK=0 NO_AFFINITY=1 USE_THREAD=0 PREFIX=`echo $LIBRARY_PREFIX | tr '\\' '/'`"
-if errorlevel 1 exit 1
-bash -lc "make test"
-if errorlevel 1 exit 1
-bash -lc "make PREFIX=`echo $LIBRARY_PREFIX | tr '\\' '/'` install"
 if errorlevel 1 exit 1
